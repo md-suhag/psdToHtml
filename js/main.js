b906399efa189a47c7a558e52041125c0f80f15a
Md Abdus Salam Suhag <108596993+md-suhag@users.noreply.github.com>
2022-12-15 22:23:41 +0600

    version 1.0

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Bx Slider
+$(document).ready(function(){
+    $('.slider-area').bxSlider();
+    $('#main-menu').meanmenu({
+        meanMenuContainer: '#mobile-menu',
+        meanScreenWidth: 991,
+    });
+  });
